a3fd1bbbcc0126be0df17a2d97be646c4ff8bae1
David Maurer <davemaurermail@gmail.com>
2017-12-04 15:10:08 -0700

    initial

---
+++ roman_numerals.rb
@@ -0,0 +1,18 @@
+class Integer
+    NUMERALS = {
+      "M" => 1,
+      "CM" => 5,
+      "X" => 10,
+
+    }
+
+  def to_roman
+    number = self
+    result = ""
+    result << NUMERALS[number]
+  end
+end
+
+# class Integer
+#   include RomanNumerals
+# end
